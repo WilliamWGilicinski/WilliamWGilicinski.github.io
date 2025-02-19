8b89addeb91010766df795741ec5bd5de704489c
William Gilicinski <william.gilicinski@gmail.com>
2025-02-18 23:53:55 -0500

    Changed layout for mobile

--- next.config.js
+++ next.config.js
@@ -1,3 +1,18 @@
+// const nextConfig = {
+//   reactStrictMode: true,
+//   webpack: (config, { isServer }) => {
+//     if (!isServer) {
+//       config.resolve.fallback = {
+//         ...config.resolve.fallback,
+//         fs: false,
+//       }
+//     }
+//     return config
+//   },
+// }
+
+// module.exports = nextConfig
+
 /**
  * @type {import('next').NextConfig}
  */
